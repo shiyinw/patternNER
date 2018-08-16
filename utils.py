fcdebe71b080bd3ba4469b4aa11048a2bf779573
SherlynW <wangsysusan@gmail.com>
2018-08-16 13:38:54 -0500

    match pattern with docu

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import re
 import nltk
 from textblob import TextBlob
 import time
+import json
 
 """
 ### Input
@@ -20,34 +21,51 @@ ground truth: /shared/data/shizhi2/data_fuzzyCRF/bio-ner/**train0_all.tsv**, **t
 patternlist: **patternlist.xlsx**
 """
 
-def pmatch(pattern, document):
+dirty = ["the", "other", "a", "an", "is", "are"]
+
+def pmatch(id, pattern, document):
     """
     This function matchs a pattern containing "CHEMICAL", "GENE", "DISEASE" to a given document
     """
 
-    res = {"content": pattern, "matches": []}
+    res = {"id":id, "content": pattern, "matches": []}
 
     # generate re query for search
     match_query = re.sub('CHEMICAL|GENE|DISEASE', "(.*?)", pattern)
     if (match_query[-5:] == "(.*?)"):
         match_query = match_query[:-5] + "(.*)"
+    if(match_query[:4] == "(.*?)"):
+        match_query = "(.*)" + match_query[4:]
 
     # search pattern in the document
     matches = re.search(match_query, document)
-    if(matches==None):
+    if(matches == None):
         return
     pos_pattern = list(re.finditer("CHEMICAL|GENE|DISEASE", pattern))
 
     index = 1
-    for p in pos_pattern[:-1]:
+    for p in pos_pattern[1:-1]:
         res["matches"].append((p.group(0), matches.group(index)))
         index += 1
-    tail = matches.group(index).split(" ")
-    if (tail[0] in ["the", "a", "in"]):
-        last_word = tail[1]
-    else:
-        last_word = tail[0]
-    res["matches"].append((pos_pattern[-1].group(0), last_word))
+    try:
+        tail = matches.group(index).split(" ")
+        index = 0
+        while(tail[index] in dirty and index<len(tail)):
+            index += 1
+        if (tail[index] not in dirty):
+            res["matches"].append((pos_pattern[-1].group(0), tail[index]))
+    except:
+        pass
+
+    try:
+        head = matches.group(index).split(" ")
+        index = -1
+        while (tail[index] in dirty and index + len(tail) >=0):
+            index -= 1
+        if (tail[index] not in dirty):
+            res["matches"].append((pos_pattern[-1].group(0), tail[index]))
+    except:
+        pass
 
     return res
 
@@ -97,8 +115,8 @@ def load_pattern(filename):
     """
     pattern = {}
     df = pd.read_excel(filename, header=None, names=["id", "pattern"])
-    for r in df.iterrows():
-        pattern[r[1]["id"]] = r[1]["pattern"]
+    for index, r in df.iterrows():
+        pattern[r["id"]] = r["pattern"]
     print("finish loading patterns: ", len(pattern))
     return pattern
 
@@ -106,8 +124,8 @@ def load_pattern(filename):
 if __name__ == "__main__":
 
 
-    res = pmatch("development of DISEASE such as GENE will DISEASE",
-           "The development of the last proteins such as we will king of all the issue")
+    res = pmatch("id00", "DISEASE therapy",
+           "the DISEASE_D013923_Thromboembolic and other complications of oral contraceptive therapy in relationship to pretreatment levels of DISEASE_D001778_blood_coagulation factors: summary report of a ten-year study.During a ten-year period, 348 SPECIES_9606_women were studied for a total of 5,877 SPECIES_9606_patient months in four separate studies relating oral contraceptives to changes in hematologic parameters.Significant increases in certain factors of the DISEASE_D001778_blood_coagulation and fibrinolysin systems (factors I,II,VII,GENE_1351_VIII,IX, and X and plasminogen) were observed in the treated groups.Severe complications developed in four SPECIES_9606_patients.")
     print("TEST pmatch   ------  ", res)
 
     ne = load_ne("180815/train1_all.tsv")  # named entities and their types
@@ -115,14 +133,25 @@ if __name__ == "__main__":
     pattern = load_pattern("180815/patternlist.xlsx") # patterns
 
 
+    matches = []
     # matching patterns
     time_start = time.time()
     cnt = 0
-    for p in pattern:
+    n = 0
+    for i, p in pattern.items():
         cnt += 1
+        print(p)
         for s in docu:
-            match = pmatch(p, s)
+            match = pmatch(i, p, s)
             if(match != None):
-                print(match)
-        if(cnt % 1e1==0):
-            print (cnt, time.time()-time_start)
+                matches.append(match)
+                n += 1
+                if(n%100==0):
+                    print(cnt, n, time.time() - time_start)
+        if(cnt % 10==0):
+            print (cnt, n, time.time()-time_start)
+
+
+
+    with open("matches1.json", "w") as f:
+        json.dump(matches, f)
